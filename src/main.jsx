65a5f908b4340c02678afca4bb31898299ceb0ab
fadoomotivation-pixel <fadoomotivation@gmail.com>
2026-02-13 13:53:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
